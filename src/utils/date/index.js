e2c1e3945d8a674848c73c31436da1364e08136f
lildworks <lildworks@gmail.com>
2020-02-14 13:11:21 +0100

    FEAT: Get now as formatted string

--- src/utils/date/index.js
+++ src/utils/date/index.js
@@ -11,3 +11,11 @@ const dateformat = require('dateformat');
 function getNow() {
     return new Date();
 }
+
+/**
+ * @param {string=} dateFormat 
+ * @returns {string} the formatted now
+ */
+function getNowFormatted(dateFormat) {
+    return dateformat(new Date(), dateFormat || configs.dateFormat);
+}
